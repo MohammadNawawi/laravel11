58b7d605924d5ab22f58d145c5760612f398b259
Mohammad Nawawi <tikp2.e24m.nawawi@gmail.com>
2024-05-09 19:21:33 +0700

    course : View Data

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,4 +1,17 @@
 <x-layout>
     <x-slot:title>{{ $title }}</x-slot:title>
-    <h1>Blog Pages</h1>
+
+    @foreach ( $blogs as $blog )
+    <article class="py-8 max-w-screen-md border-b border-gray-300">
+      <a href="/blog/{{ $blog['slug'] }}" class="hover:text-emerald-400">
+        <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">{{ $blog['title'] }}</h2>
+      </a>
+      <div class="text-base text-gray-500">
+        <a href="#">{{ $blog['author'] }} | {{ date('d M Y') }}</a>
+      </div>
+      <p class="my-4 font-light">{{ Str::limit($blog['body'], 200) }}</p>
+      <a href="/blog/{{ $blog['slug'] }}" class="font-medium text-blue-500 hover:text-emerald-400">Read more &raquo;</a>
+    </article>
+    @endforeach
+
   </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -7,7 +8,47 @@ Route::get('/', function () {
 });
 
 Route::get('/blog', function () {
-    return view('blog', ['title' => 'Blog Page']);
+    return view('blog', ['title' => 'Blog Page', 'blogs' => [
+        [
+            'id' => 1,
+            'slug' => 'blog-1',
+            'title' => 'Blog 1',
+            'author' => 'Author 1',
+            'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste odit praesentium saepe. Velit quaerat reprehenderit, tempora inventore aliquam, molestiae incidunt mollitia unde suscipit harum possimus maxime voluptatibus delectus quis cupiditate?',
+        ],
+        [
+            'id' => 2,
+            'slug' => 'blog-2',
+            'title' => 'Blog 2',
+            'author' => 'Author 2',
+            'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste odit praesentium saepe. Velit quaerat reprehenderit, tempora inventore aliquam, molestiae incidunt mollitia unde suscipit harum possimus maxime voluptatibus delectus quis cupiditate?',
+        ],
+    ]]);
+});
+
+Route::get('/blog/{slug}', function ($slug) {
+    $posts = [
+        [
+            'id' => 1,
+            'slug' => 'blog-1',
+            'title' => 'Blog 1',
+            'author' => 'Author 1',
+            'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste odit praesentium saepe. Velit quaerat reprehenderit, tempora inventore aliquam, molestiae incidunt mollitia unde suscipit harum possimus maxime voluptatibus delectus quis cupiditate?',
+        ],
+        [
+            'id' => 2,
+            'slug' => 'blog-2',
+            'title' => 'Blog 2',
+            'author' => 'Author 2',
+            'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste odit praesentium saepe. Velit quaerat reprehenderit, tempora inventore aliquam, molestiae incidunt mollitia unde suscipit harum possimus maxime voluptatibus delectus quis cupiditate?',
+        ],
+    ];
+
+    $post = Arr::first($posts, function ($post) use ($slug) {
+        return $post['slug'] == $slug;
+    });
+
+    return view('post', ['title' => $post['title'], 'post' => $post]);
 });
 
 Route::get('/about', function () {
